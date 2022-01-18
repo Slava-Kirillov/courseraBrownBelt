5053fe51fd3646b9dfdd86c05f12aaaa320ff31a
vkirillov <viacheslav.kirillov@center2m.ru>
2022-01-18 13:27:28 +0300

    add main func

---
+++ task_1/main.cpp
@@ -0,0 +1,18 @@
+#include "test_runner.h"
+#include "profile.h"
+
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <random>
+#include <thread>
+
+using namespace std;
+
+int main() {
+    TestRunner tr;
+}
