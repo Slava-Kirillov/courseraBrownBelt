e00ba3ea072f2056e0fad31f23bc832deca8b287
vkirillov <viacheslav.kirillov@center2m.ru>
2022-02-03 12:11:41 +0300

    task 4

--- task_4/main.cpp
+++ task_4/main.cpp
@@ -1,4 +1,5 @@
 #include "test_runner.h"
+#include "profile.h"
 
 #include <iostream>
 #include <map>
@@ -13,67 +14,46 @@ struct Record {
     string user;
     int timestamp;
     int karma;
+};
 
-    bool operator<(const Record &record) const {
-        return this->id < record.id;
-    }
+using multiMapIntIter = multimap<int, const Record*>::const_iterator;
+using multiMapStringIter = multimap<string, const Record*>::const_iterator;
 
-    bool operator==(const Record &record) const {
-        return this->id == record.id &&
-               this->title == record.title &&
-               this->user == record.user &&
-               this->timestamp == record.timestamp &&
-               this->karma == record.karma;
-    }
+struct RecordMultiMap {
+    const Record* record;
+    multiMapIntIter time_it;
+    multiMapIntIter karma_it;
+    multiMapStringIter user_it;
 };
 
 class Database {
 public:
     bool Put(const Record &record) {
-        if (id_map.find(record.id) != id_map.end()) {
-            return false;
+        auto [pos, YES] = data.try_emplace(record.id, RecordMultiMap{&record, {}, {}, {}});
+
+        if (YES) {
+            pos->second.karma_it = karma_index.insert({record.karma, &record});
+            pos->second.time_it = time_index.insert({record.timestamp, &record});
+            pos->second.user_it = user_index.insert({record.user, &record});
         }
-
-        id_map[record.id] = &record;
-        time_map.insert(make_pair(record.timestamp, &record.id));
-        karma_map.insert(make_pair(record.karma, &record.id));
-        user_map.insert(make_pair(record.user, &record.id));
-
-        return true;
+        return YES;
     }
 
     const Record *GetById(const string &id) const {
-        auto it = id_map.find(id);
-        if (it != id_map.end()) {
-            return it->second;
+        auto it = data.find(id);
+        if (it != data.end()) {
+            return it->second.record;
         }
         return nullptr;
     }
 
     bool Erase(const string &id) {
-        auto it = id_map.find(id);
-        if (it != id_map.end()) {
-            auto time_it = time_map.find(it->second->timestamp);
-            while (*time_it->second != it->first) {
-                time_it++;
-            }
-
-            auto karma_it = karma_map.find(it->second->karma);
-            while (*karma_it->second != it->first) {
-                karma_it++;
-            }
-
-            auto user_it = user_map.find(it->second->user);
-            while (*user_it->second != it->first) {
-                user_it++;
-            }
-
-            time_map.erase(time_it);
-            karma_map.erase(karma_it);
-            user_map.erase(user_it);
-
-            id_map.erase(it);
-
+        auto it = data.find(id);
+        if (it != data.end()) {
+            time_index.erase(it->second.time_it);
+            karma_index.erase(it->second.karma_it);
+            user_index.erase(it->second.user_it);
+            data.erase(it->second.record->id);
             return true;
         }
         return false;
@@ -81,47 +61,37 @@ public:
 
     template<typename Callback>
     void RangeByTimestamp(int low, int high, Callback callback) const {
-        for (int timestamp = low; timestamp <= high; timestamp++) {
-            auto it = time_map.find(timestamp);
-            while (it != time_map.end() && it->first == timestamp) {
-                if (!callback(*id_map.find(*it->second)->second)) {
-                    return;
-                }
-                it++;
+        for(auto it = time_index.lower_bound(low); it != time_index.upper_bound(high); it++) {
+            if (!callback(*it->second)) {
+                return;
             }
         }
     }
 
     template<typename Callback>
     void RangeByKarma(int low, int high, Callback callback) const {
-        for (int karma = low; karma <= high; karma++) {
-            auto it = karma_map.find(karma);
-            while (it != karma_map.end() && it->first == karma) {
-                if (!callback(*id_map.find(*it->second)->second)) {
-                    return;
-                }
-                it++;
+        for(auto it = karma_index.lower_bound(low); it != karma_index.upper_bound(high); it++) {
+            if (!callback(*it->second)) {
+                return;
             }
         }
     }
 
     template<typename Callback>
     void AllByUser(const string &user, Callback callback) const {
-        auto it = user_map.find(user);
-        while (it != user_map.end() && it->first == user) {
-            if (!callback(*id_map.find(*it->second)->second)) {
+        for(auto it = user_index.lower_bound(user); it != user_index.upper_bound(user); it++) {
+            if (!callback(*it->second)) {
                 return;
             }
-            it++;
         }
     }
 
 private:
-    unordered_map<string, const Record*> id_map;
+    unordered_map<string, RecordMultiMap> data;
 
-    multimap<int, const string*> time_map;
-    multimap<int, const string*> karma_map;
-    multimap<string, const string*> user_map;
+    multimap<int, const Record*, hash<int>> time_index;
+    multimap<int, const Record*, hash<int>> karma_index;
+    multimap<string, const Record*, hash<string>> user_index;
 };
 
 void TestRangeBoundaries() {
@@ -131,9 +101,13 @@ void TestRangeBoundaries() {
     Database db;
     db.Put({"id1", "Hello there", "master", 1536107260, good_karma});
     db.Put({"id2", "O>>-<", "general2", 1536107260, bad_karma});
+    db.Put({"id3", "O>>-<", "general2", 1536107260, 0});
 
     int count = 0;
-    db.RangeByKarma(bad_karma, good_karma, [&count](const Record &) {
+    db.RangeByKarma(bad_karma, good_karma, [&count](const Record & record) {
+        if (record.id == "id1") {
+            return false;
+        }
         ++count;
         return true;
     });
@@ -141,10 +115,33 @@ void TestRangeBoundaries() {
     ASSERT_EQUAL(2, count);
 }
 
+void TestRangeTime() {
+    const int good_karma = 1000;
+    const int bad_karma = -10;
+
+    Database db;
+    db.Put({"id1", "Hello there", "master", 1536107260, good_karma});
+    db.Put({"id2", "O>>-<", "general2", 1536107360, bad_karma});
+    db.Put({"id3", "O>>-<", "general2", 1536107460, 0});
+
+    int count = 0;
+    db.RangeByTimestamp(1536107259, 1536107460, [&count](const Record & record) {
+        ++count;
+        return true;
+    });
+
+    ASSERT_EQUAL(3, count);
+}
+
 void TestSameUser() {
     Database db;
     db.Put({"id1", "Don't sell", "master", 1536107260, 1000});
     db.Put({"id2", "Rethink life", "master", 1536107260, 2000});
+    db.Put({"id3", "Rethink life", "master1", 1536107260, 2000});
+    db.Put({"id4", "Rethink life", "maste", 1536107260, 2000});
+    db.Put({"id5", "Rethink life", "master", 1536107260, 2000});
+    db.Put({"id6", "Rethink life", "aster", 1536107260, 2000});
+    db.Put({"id5", "Rethink life", " master ", 1536107260, 2000});
 
     int count = 0;
     db.AllByUser("master", [&count](const Record &) {
@@ -152,7 +149,7 @@ void TestSameUser() {
         return true;
     });
 
-    ASSERT_EQUAL(2, count);
+    ASSERT_EQUAL(3, count);
 }
 
 void TestReplacement() {
@@ -164,8 +161,12 @@ void TestReplacement() {
     db.Put({"id", final_body, "not-master", 1536107260, -10});
 
     auto record = db.GetById("id");
-    ASSERT(record != nullptr);
-    ASSERT_EQUAL(final_body, record->title);
+
+    cout << (record != nullptr) << endl;
+    cout << (record->title == final_body) << endl;
+
+//    ASSERT(record != nullptr);
+//    ASSERT_EQUAL(final_body, record->title);
 }
 
 int main() {
@@ -173,5 +174,6 @@ int main() {
     RUN_TEST(tr, TestRangeBoundaries);
     RUN_TEST(tr, TestSameUser);
     RUN_TEST(tr, TestReplacement);
+    RUN_TEST(tr, TestRangeTime);
     return 0;
 }
